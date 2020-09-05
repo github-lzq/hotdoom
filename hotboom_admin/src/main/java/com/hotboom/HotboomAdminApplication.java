6f8ba676c9e4decc52ae3d385f53c9a785699fea
github-lzq <luzuquan0915@126.com>
2020-09-05 10:52:33 +0800

    test

---
+++ hotboom_admin/src/main/java/com/hotboom/HotboomAdminApplication.java
@@ -0,0 +1,13 @@
+package com.hotboom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotboomAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotboomAdminApplication.class, args);
+    }
+
+}
---
+++ hotboom_admin/src/test/java/com/hotboom/HotboomAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hotboom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotboomAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
